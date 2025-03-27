3a82b341e3b9ba43bc3844f59102c0e432217206
David Justice <david@devigned.com>
2025-03-27 10:48:36 -0400

    remove types
    Signed-off-by: David Justice <david@devigned.com>

--- apps/compose/app-js/types/interfaces/example-domain-adder.d.ts
+++
@@ -1,2 +0,0 @@
-/** @module Interface example:domain/adder@0.3.0 **/
-export function add(x: number, y: number): number;
--- apps/compose/app-js/types/interfaces/example-domain-chat.d.ts
+++
@@ -1,5 +0,0 @@
-/** @module Interface example:domain/chat@0.3.0 **/
-export function chat(request: ChatRequest): ChatResponse;
-export type ChatRequest = import('./example-domain-types.js').ChatRequest;
-export type ChatResponse = import('./example-domain-types.js').ChatResponse;
-export type Error = import('./example-domain-types.js').Error;
